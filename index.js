a6d51c4967bb163e38b3ad65f8acfbec13d372f1
Jochem Kuipers <jochem@kuipers.cc>
2026-05-11 13:16:35 +0200

    setup get route for contact object setup get route for update page setup post route for updating contact object

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get("/", async (req, res) => {
+  const contacts = "https://api.hubspot.com/crm/v3/objects/contacts";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(contacts, { headers });
+    const data = resp.data.results;
+    res.render("contacts", { title: "Contacts | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-// * Code for Route 1 goes here
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum.",
+  });
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
